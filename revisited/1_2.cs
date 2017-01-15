c470204257efe89a6dbcedf28136b1e2ce60fd76
bnoden <brandroid369@gmail.com>
2017-01-14 22:49:01 -0600

    .

---
+++ revisited/1_2.cs
@@ -0,0 +1,23 @@
+/*2. Carbon-14 dating is a common technique for measuring the approximate age of matter that was 
+once living. The formula for measuring time base on Carbon-14 decay is
+t = (T / ln(2) * ln(1 + D / P)) where T is the half-life of Carbon-14
+(5730 years), ln(2) is natural logarithm of 2, D is the amount of carbon-14 fund in a given dead sample, and 
+P is the amount of Carbon-14 in a living specimen today. Write a C# program that asks the user input the value of 
+D/P (such as 0.5) for a given sample and outputs the approximate age of the sample in years.
+Use a named constant and a method in the problem. Output the result to two decimal places.
+
+Sample Run:  
+This program calculates the age of a dead sample based on the ratio of 
+Carbon 14 in a dead sample.
+Enter the ratio D/P: 
+.05   
+Given a D/P ratio of 0.05 for C14, the age of the sample is 403.33 years.
+ */
+
+using System;
+
+class Program {
+    static void Main(string[] args) {
+
+    }
+};
