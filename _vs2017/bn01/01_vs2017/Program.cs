f229d50e0373fed1769a3854f0d4dfb0ccd62d0a
bnoden <brandroid369@gmail.com>
2017-04-20 23:35:22 -0500

    .

--- _vs2017/bn01/01_vs2017/Program.cs
+++ _vs2017/bn01/01_vs2017/Program.cs
@@ -4,12 +4,31 @@ namespace _01_vs2017
 {
     class Program
     {
+        private string user {get; set;}
+        private string pwd {get; set;}
+        const int PWD_MAX = 8;
+
         static void Main(string[] args)
         {
-            string user;
-            Console.Write("username: ");
-            user = Console.ReadLine();
-            Console.WriteLine("Hello, "+user);
+            Program p = new Program();
+
+            Console.Write("user name: ");
+            p.user = Console.ReadLine();
+            p.pwd = "";
+            Console.Write("password: ");
+            
+            while ((p.pwd).Length <= PWD_MAX) {
+                Console.ReadKey(true);
+                p.pwd+="*";
+                Console.Write("*");
+            }
+
+            Console.ReadKey();
+            Console.WriteLine("\nHello, "+p.user);
+
+
+            Console.ReadKey();
+            return;
         }
     }
 }
