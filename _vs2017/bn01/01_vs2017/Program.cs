7136059a32fabb64afd20507762bd7619835c6df
bnoden <brandroid369@gmail.com>
2017-04-20 20:20:08 -0500

    .

---
+++ _vs2017/bn01/01_vs2017/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01_vs2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string user;
+            Console.Write("username: ");
+            user = Console.ReadLine();
+            Console.WriteLine("Hello, "+user);
+        }
+    }
+}
---
+++ test_cli.cs
@@ -0,0 +1,5 @@
+class SeeSharp {
+	static void Main() {
+		System.Console.WriteLine("learn cli");
+	}
+}
