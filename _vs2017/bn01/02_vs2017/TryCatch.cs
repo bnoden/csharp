3908587e638116476264c239ff7f5ce706c5e34d
bnoden <brandroid369@gmail.com>
2017-04-22 00:33:55 -0500

    .

---
+++ _vs2017/bn01/02_vs2017/TryCatch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+namespace _02_vs2017
+{
+    class TryCatch
+    {
+        static void Main(string[] args)
+        {
+            string path = @"res";
+
+            FileStream file = null;
+            StreamWriter writer = null;
+            try {
+                if (Directory.Exists(path)) {
+                    Write("File name: ");
+                    string filename = ReadLine();
+                    file = File.OpenWrite(Path.Combine(path, filename));
+                    writer = new StreamWriter(file);
+
+                    Write("Comment: ");
+                    string comment = ReadLine();
+                    var date = DateTime.Now;
+                    
+                    writer.WriteLine("// "+date.ToString()+" ...Success!");
+                    writer.WriteLine("// "+comment);
+                } else {
+                    WriteLine($"{path} could not be found...");
+                }
+            } catch (Exception ex) {
+                WriteLine($"{ex.GetType()} says {ex.Message}");
+            } finally {
+                if (writer != null) {
+                    writer.Dispose();
+                    WriteLine("The writer's unmanaged resources were disposed.");
+                }
+                if (file != null) {
+                    file.Dispose();
+                    WriteLine("The file's unmanaged resources were disposed.");
+                }
+            }
+
+
+
+            Console.ReadKey();
+            return;
+        }
+    }
+}
