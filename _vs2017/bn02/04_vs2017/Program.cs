2ef7a78c339b1d20a6c38382031285336700aedd
bnoden <brandroid369@gmail.com>
2017-04-23 12:09:09 -0500

    .

--- _vs2017/bn02/04_vs2017/Program.cs
+++ _vs2017/bn02/04_vs2017/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Xml.Linq;
+using System.Text.RegularExpressions;
 using static System.Console;
 
 namespace _04_vs2017
@@ -10,33 +11,66 @@ namespace _04_vs2017
         {
             var doc = new XDocument();
 
-            string city = "";
-
-            while (city.Length < 1) {
-                Write("Enter city name: ");
-                city = ReadLine();
+            string isaidno = "";
+            const int MAX_ATTEMPTS = 3;
+            int attempt = 1;
+            var idCheck = new Regex(@"^[0-9]{10}$");
+            DateTime dt;
+            while(!idCheck.IsMatch(isaidno)) {
+                if (attempt>1) { WriteLine($"Attempt {attempt} of {MAX_ATTEMPTS}"); }
+                
+                Write("10-digit International Secret Agent ID Number: ");
+                isaidno = ReadLine();
+                attempt++;
+                if (!(attempt<=MAX_ATTEMPTS)) {
+                    dt = DateTime.Now;
+                    WriteLine($"Verification bypassed {dt}");
+                    WriteLine("Warning: Connection is not secure and may be monitored.");
+                    isaidno = "0123456789";
+                }
             }
-            string[] citySplit = city.Split(' ');
-            string cityJoin = "";
-            foreach (string word in citySplit) {
-                    cityJoin+=word;
+
+            string c = "";
+
+            while (c.Length < 1) {
+                Write("Enter city name: ");
+                c = ReadLine();
+            }
+            string[] cSplit = c.Split(' ');
+            string cJoin = "";
+            foreach (string word in cSplit) {
+                    cJoin+=word;
                 }
             //WriteLine(cityJoin);
-            WriteLine($"\nThere are {cityJoin.Length} letters in {city}.");
-            WriteLine($"The first char is '{city[0]}' and the last char is '{city[city.Length-1]}'.");
-            if (cityJoin.Length > 2 && cityJoin.Length%2!=0) {
-                WriteLine($"The middle char is '{cityJoin[cityJoin.Length/2]}'.");
-            } else if (cityJoin.Length > 3 && cityJoin.Length%2==0) {
-                WriteLine($"The middle chars are '{cityJoin[(cityJoin.Length/2)-1]}' and '{cityJoin[cityJoin.Length/2]}'.");
+            WriteLine($"\nThere are {cJoin.Length} letters in {c}.");
+            WriteLine($"The first letter is '{c[0]}' and the last letter is '{c[c.Length-1]}'.");
+            if (cJoin.Length > 2 && cJoin.Length%2!=0) {
+                WriteLine($"The middle letter is '{cJoin[cJoin.Length/2]}'.");
+            } else if (cJoin.Length > 3 && cJoin.Length%2==0) {
+                WriteLine($"The middle letters are '{cJoin[(cJoin.Length/2)-1]}' and '{cJoin[cJoin.Length/2]}'.");
             }
 
-            Write("Enter full name (first and last): ");
+            bool success = true;
+            Write("\nEnter full name (first and last): ");
             string fullname = ReadLine();
-            int indexOfTheSpace = fullname.IndexOf(' ');
-            string firstname = fullname.Substring(0, indexOfTheSpace);
-            string lastname = fullname.Substring(indexOfTheSpace + 1);
-            string found = firstname.Length>lastname.Length? "was" : "could not be";
-            WriteLine($"{lastname}, {firstname} {found} found in {city}");
+            
+            try {
+                int indexOfTheSpace = fullname.IndexOf(' ');
+                string firstname = fullname.Substring(0, indexOfTheSpace);
+                string lastname = fullname.Substring(indexOfTheSpace + 1);
+                string found = firstname.Length>lastname.Length? "inside" : "outside of";
+            WriteLine($"{lastname}, {firstname} is currently {found} {c}");
+            } catch (ArgumentOutOfRangeException) {
+                success = false;
+            } finally {
+                if (!success) {
+                    dt = DateTime.Now;
+                    WriteLine($"Invalid input. Attempt logged {dt}");
+                }
+                WriteLine("Goodbye.");
+            }
+
+            
 
             return;
             
