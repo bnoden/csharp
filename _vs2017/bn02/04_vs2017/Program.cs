7a3b8478064af2b973319d8dca4612132483b701
bnoden <brandroid369@gmail.com>
2017-04-23 20:25:03 -0500

    .

--- _vs2017/bn02/04_vs2017/Program.cs
+++ _vs2017/bn02/04_vs2017/Program.cs
@@ -10,8 +10,9 @@ namespace _04_vs2017
     {
         static void Main(string[] args)
         {
+            DateTime msg = DateTime.Now;
             var doc = new XDocument();
-            const int MAX_ATTEMPTS = 3;
+            const int MAX_ATTEMPTS = 3, toBin = 2, toHex = 16;
             string isaidno = "";
             
             int attempt = 1;
@@ -25,44 +26,48 @@ namespace _04_vs2017
                 attempt++;
                 if (!(attempt<=MAX_ATTEMPTS)) {
                     
-                    WriteLine("\nTop-level supersecret agent has implementd [BITSTRIPE] protocol.\n");
-
-                    int toBin = 2;
-                    int toHex = 16;
-                    
+                    WriteLine("\nMAX_ATTEMPTS reached! Implementing [BITSTRIPE] emergency entry protocol.\n");
+                    ReadKey();
+                    string bitstripe = "";
                     for (int b = 128; b < 255; b++) {
-                        Write("0b"+Convert.ToString(b, toBin)
-                            +" 0x"+Convert.ToString(b*16, toHex)+' ');
+                        bitstripe=("0b"+Convert.ToString(b, toBin)+" 0x"+Convert.ToString(b*16, toHex)+' ');
+                        Write(bitstripe);
                     }
                     dt = DateTime.Now;
+                    ReadKey();
                     WriteLine($"\n\nVerification bypassed {dt}");
+                    ReadKey();
                     WriteLine("Warning: Connection is not secure and may be monitored.");
                     isaidno = "0123456789";
+                    ReadKey();
                 }
             }
 
-            string c = "";
+            string city = "";
 
-            while (c.Length < 1) {
+            while (city.Length < 1) {
                 Write("Enter city name: ");
-                c = ReadLine();
+                city = ReadLine();
             }
-            string[] cSplit = c.Split(' ');
-            string cJoin = "";
-            foreach (string word in cSplit) {
-                    cJoin+=word;
+            string[] citySplit = city.Split(' ','.','-','\'');
+            string cityJoin = "";
+            foreach (string word in citySplit) {
+                    cityJoin+=word;
                 }
-            //WriteLine(cityJoin);
-            WriteLine($"\nThere are {cJoin.Length} letters in {c}.");
-            WriteLine($"The first letter is '{c[0]}' and the last letter is '{c[c.Length-1]}'.");
-            if (cJoin.Length > 2 && cJoin.Length%2!=0) {
-                WriteLine($"The middle letter is '{cJoin[cJoin.Length/2]}'.");
-            } else if (cJoin.Length > 3 && cJoin.Length%2==0) {
-                WriteLine($"The middle letters are '{cJoin[(cJoin.Length/2)-1]}' and '{cJoin[cJoin.Length/2]}'.");
-            }
 
+            WriteLine($"\nThere are {cityJoin.Length} letters in {city}.");
+            ReadKey();
+            WriteLine($"The first letter is '{city[0]}' and the last letter is '{city[city.Length-1]}'.");
+            ReadKey();
+            if (cityJoin.Length > 2 && cityJoin.Length%2!=0) {
+                WriteLine($"The middle letter is '{cityJoin[cityJoin.Length/2]}'.");
+            }
+            else if (cityJoin.Length > 3 && cityJoin.Length%2==0) {
+                WriteLine($"The middle letters are '{cityJoin[(cityJoin.Length/2)-1]}' and '{cityJoin[cityJoin.Length/2]}'.");
+            }
+            ReadKey();
             bool success = true;
-            Write("\nEnter full name (first and last): ");
+            Write("\nEnter target's full name (first and last): ");
             string fullname = ReadLine();
             
             try {
@@ -70,17 +75,44 @@ namespace _04_vs2017
                 string firstname = fullname.Substring(0, indexOfTheSpace);
                 string lastname = fullname.Substring(indexOfTheSpace + 1);
                 string found = firstname.Length>lastname.Length? "inside" : "outside of";
-            WriteLine($"{lastname}, {firstname} is currently {found} {c}");
+                WriteLine($"{lastname}, {firstname} is currently {found} {city}");
             } catch (ArgumentOutOfRangeException) {
                 success = false;
             } finally {
                 if (!success) {
                     dt = DateTime.Now;
                     WriteLine($"Invalid input. Attempt logged {dt}");
+                    ReadKey();
                 }
-                WriteLine("Goodbye.");
+                WriteLine("Wait...");
+                ReadKey();
             }
 
+            WriteLine("\nMessages:\n");
+            ReadKey();
+            WriteLine($"{msg}\t From: Agent 'Gray' #1C0B9AE9A3, DBOZ Specialist");
+            ReadKey();
+            WriteLine("The latest .NET dictionary update has been released.\n"+
+                "Classification: RMD (read memorize destroy)\n");
+            ReadKey();
+
+            var keywords = new System.Collections.Generic.Dictionary<string, string>();
+            keywords.Add("int", "32-bit integer data type"); 
+            keywords.Add("long", "64-bit integer data type"); 
+            keywords.Add("float", "Single precision floating point number"); 
+            WriteLine("Keywords and their definitions"); 
+            foreach (System.Collections.Generic.KeyValuePair<string, string> item in keywords) 
+            { 
+              WriteLine($"  {item.Key}: {item.Value}"); 
+            } 
+            WriteLine($"The definition of long is {keywords["long"]}"); 
+            ReadKey();
+
+            WriteLine("That is all. Formtting disk...");
+            ReadKey();
+            WriteLine("...");
+            ReadKey();
+            WriteLine("...OK");
             
 
             return;
