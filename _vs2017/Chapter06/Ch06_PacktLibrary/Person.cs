495de646970942c7d49a33ed58ed154d3aa2fac0
bnoden <brandroid369@gmail.com>
2017-04-25 16:23:47 -0500

    .

--- _vs2017/Chapter06/Ch06_PacktLibrary/Person.cs
+++ _vs2017/Chapter06/Ch06_PacktLibrary/Person.cs
@@ -30,5 +30,13 @@ namespace Packt.CS7
             WriteLine($"{this.Name} was instantiated at {this.Instantiated:hh:mm:ss} on {this.Instantiated:dddd, d MMMM yyyy}");
             ReadLine();
             }
+        public string GetOrigin(DateTime dt) {
+            dt = this.DateOfBirth;
+            return $"{this.Name} was born on {dt:dddd, d MMMM yyyy}";
+        }
+        public string GetOrigin(string hp) {
+            hp = this.HomePlanet;
+            return $"{this.Name} comes from {hp}";
+        }
     }
 }
--- _vs2017/Chapter06/Ch06_PeopleApp/Program.cs
+++ _vs2017/Chapter06/Ch06_PeopleApp/Program.cs
@@ -17,19 +17,22 @@ namespace Ch06_PeopleApp
             var p1 = new Person();
             p1.Name = "Don Jones";
             p1.DateOfBirth = new DateTime(1947, 10, 26);
+            var datetime = new DateTime();
             p1.inst();
-            WriteLine($"{p1.Name} was born on {p1.DateOfBirth:dddd, d MMMM yyyy}");
+            WriteLine(p1.GetOrigin(datetime));
             ReadLine();
             var p2 = new Person {
                 Name = "David Abrahamowitz",
                 DateOfBirth = new DateTime(1934, 7, 6)
             };
             p2.inst();
-            WriteLine($"{p2.Name} was born on {p2.DateOfBirth:d MMM yy}");
+            WriteLine(p2.GetOrigin(datetime));
             ReadLine();
+
             p1.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
             WriteLine($"{p1.Name}'s favorite wonder is {p1.FavoriteAncientWonder}");
             ReadLine();
+
             p1.BucketList = WondersOfTheAncientWorld.HangingGardensOfBabylon |
                 WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
             //p1.BucketList = (WondersOfTheAncientWorld)18;
@@ -39,18 +42,21 @@ namespace Ch06_PeopleApp
             p1.Children.Add(new Person());
             WriteLine($"{p1.Name} has {p1.Children.Count} children.");
             ReadLine();
-            WriteLine($"{p1.Name} comes from {p1.HomePlanet}");
-            WriteLine($"{p2.Name} comes from {p2.HomePlanet}");
+
+            string homeplanet = "";
+            WriteLine(p1.GetOrigin(homeplanet));
+            WriteLine(p2.GetOrigin(homeplanet));
             ReadLine();
+
             BankAccount.InterestRate = 0.012M;
-            var ba1 = new BankAccount();
-            ba1.AccountName = "Mr. Abrahamowitz";
-            ba1.Balance = 21387.16M;
-            WriteLine($"{ba1.AccountName} earned {ba1.Balance*BankAccount.InterestRate:C} interest.");
-            var ba2 = new BankAccount();
-            ba2.AccountName = "Mr. Morris";
-            ba2.Balance = 62.48M;
-            WriteLine($"{ba2.AccountName} earned {ba2.Balance*BankAccount.InterestRate:C} interest.");
+            var acct1 = new BankAccount();
+            acct1.AccountName = "Mr. Abrahamowitz";
+            acct1.Balance = 21387.16M;
+            WriteLine($"{acct1.AccountName} earned {acct1.Balance*BankAccount.InterestRate:C} interest.");
+            var acct2 = new BankAccount();
+            acct2.AccountName = "Mr. Morris";
+            acct2.Balance = 62.48M;
+            WriteLine($"{acct2.AccountName} earned {acct2.Balance*BankAccount.InterestRate:C} interest.");
             ReadLine();
             
             var p3 = new Person("Alfador");
