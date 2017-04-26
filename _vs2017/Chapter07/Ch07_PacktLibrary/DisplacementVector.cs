1b366f6ac632a4b4face5eef01c73ca3b573f548
bnoden <brandroid369@gmail.com>
2017-04-26 13:55:00 -0500

    .

---
+++ _vs2017/Chapter07/Ch07_PacktLibrary/DisplacementVector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Packt.CS7
+{
+    public struct DisplacementVector
+    {
+        public int X,Y;
+
+        public DisplacementVector(int initialX, int initialY) {
+            X = initialX;
+            Y = initialY;
+        }
+        public static DisplacementVector operator+ (
+            DisplacementVector v1, DisplacementVector v2) {
+            return new DisplacementVector(v1.X+v2.X, v1.Y+v2.Y);
+        }
+    }
+}
