6e6715749416c85d46db94b9990f21985623b993
bnoden <brandroid369@gmail.com>
2017-04-26 02:27:18 -0500

    .

---
+++ _vs2017/Chapter07/Ch07_PacktLibrary/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace Packt.CS7
+{
+    public class Person
+    {
+
+        // fields
+        public string Name;
+        public DateTime DateOfBirth;
+        public List<Person> Children = new List<Person>();
+
+        // methods
+        public void WriteToConsole() {
+            WriteLine($"{Name} was born on {DateOfBirth:dddd, d MMMM yyyy}");
+        }
+        public Person HoldHands(Person partner) {
+            var progeny = new Person { Name = $"Fresh baby of {Name} and {partner.Name}" };
+            Children.Add(progeny);
+            partner.Children.Add(progeny);
+            return progeny;
+        }
+
+    }
+}
---
+++ _vs2017/Chapter07/Ch07_PeopleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Packt.CS7;
+using static System.Console;
+
+namespace Ch07_PeopleApp
+{
+    class Program
+    {
+        static string ren(Person p) {
+            return p.Children.Count == 1 ? "child" : "children";
+        }
+        static void Main(string[] args)
+        {
+            var phil = new Person { Name = "Uncle Phil" };
+            var viv = new Person { Name = "Aunt Viv" };
+            var progeny1 = phil.HoldHands(viv);
+            
+            WriteLine($"{viv.Name} has {viv.Children.Count} {ren(viv)}.");
+            WriteLine($"{phil.Name} has {phil.Children.Count} {ren(phil)}.");
+            WriteLine($"{phil.Name}'s first child is named \"{phil.Children[0].Name}\".");
+
+        }
+    }
+}
