c54df821fddeb06d72d1d91b6746a350c7366d32
bnoden <brandroid369@gmail.com>
2017-04-28 18:30:23 -0500

    .

--- _vs2017/Chapter10/Ch10_FileSystem/Program.cs
+++ _vs2017/Chapter10/Ch10_FileSystem/Program.cs
@@ -9,19 +9,29 @@ namespace Ch10_FileSystem
     {
         static void Main(string[] args)
         {
-            string root = @"D:\000\0csharp\_vs2017\";
-            string dir = $"{root}Ch10_Example";
+            string root = @"D:/000/0csharp/_vs2017/";
+            //string dir = $"{root}Ch10_Example";
+            string dir = "../Ch10_Example/";
 
             WriteLine($"{dir} exists? {Exists(dir)}");
+            ReadLine();
             WriteLine($"Creating {dir}");
             CreateDirectory(dir);
+            ReadLine();
             WriteLine($"{dir} exists? {Exists(dir)}");
             WriteLine($"Deleting {dir}");
             Delete(dir);
+            ReadLine();
             WriteLine($"{dir} exists? {Exists(dir)}");
+            WriteLine();
 
-            string textFile = $"{root}Ch10.txt";
-            string backupFile = $"{root}Ch10.bak";
+            WriteLine($"Creating {dir}");
+            CreateDirectory(dir);
+            string dirBackup = "../backup/";
+            CreateDirectory(dirBackup);
+
+            string textFile = $"{dir}Ch10.txt";
+            string backupFile = $"{dirBackup}Ch10.bak";
             
             WriteLine($"{textFile} exists? {File.Exists(textFile)}");
 
@@ -39,6 +49,22 @@ namespace Ch10_FileSystem
             StreamReader textReader = File.OpenText(backupFile);
             WriteLine(textReader.ReadToEnd());
             textReader.Dispose();
+
+            WriteLine($"File name: {Path.GetFileName(textFile)}");
+            WriteLine($"File name without extension: " +
+                $"{Path.GetFileNameWithoutExtension(textFile)}");
+            WriteLine($"File extension: {Path.GetExtension(textFile)}");
+            WriteLine($"Random file name: {Path.GetRandomFileName()}");
+            WriteLine($"Temporary file name: {Path.GetTempFileName()}");
+            WriteLine();
+
+            //string backup = $"{root}Ch10.bak";
+            var info = new FileInfo(backupFile);
+            WriteLine($"{backupFile} contains {info.Length} bytes.");
+            WriteLine($"{backupFile} was last accessed {info.LastAccessTime}");
+            WriteLine($"{backupFile} has readonly set to {info.IsReadOnly}");
+
+            Delete(dir);
 
         }
     }
