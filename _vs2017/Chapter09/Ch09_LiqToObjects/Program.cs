975b602760c210111beed3ef2d58644bebe50fc5
bnoden <brandroid369@gmail.com>
2017-04-28 03:47:07 -0500

    .

--- _vs2017/Chapter09/Ch09_LiqToObjects/Program.cs
+++ _vs2017/Chapter09/Ch09_LiqToObjects/Program.cs
@@ -1,21 +1,44 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Generic; // for IEnumerable<T>
+using System.Linq; // for LINQ extension methods
+
 using static System.Console;
 
-namespace Ch09_LinqToObjects
+namespace Ch09_Sets
 {
     class Program
     {
+        private static void Output(
+            IEnumerable<string> cohort, string description = "") {
+            if (!string.IsNullOrEmpty(description)) { WriteLine(description); }
+            Write("  ");
+            WriteLine(string.Join(", ", cohort.ToArray()));
+        }
+
         static void Main(string[] args)
         {
-            var names = new string[] {"Pam", "Michael", "Jim", "Dwight", "Angela",
-                "Kevin", "Toby", "Creed"};
+            var cohort1 = new string[] { "Rachel", "Gareth", "Jonathan", "George" };
+            var cohort2 = new string[] { "Jack", "Stephen", "Daniel", "Jack", "Jared" };
+            var cohort3 = new string[] { "Declan", "Jack", "Jack", "Jasmine", "Conor" };
 
-            var query = names
-                .Where(name => name.Length>4)
-                .OrderBy(name => name.Length)
-                .ThenBy(name => name);
-            foreach (var item in query) { WriteLine(item); }
+            Output(cohort1, "Cohort 1");
+            Output(cohort2, "Cohort 2");
+            Output(cohort3, "Cohort 3");
+            WriteLine();
+
+            Output(cohort2.Distinct(), "cohort2.Distinct(): removes duplicates\n");
+            Output(cohort2.Union(cohort3),
+                "cohort2.Union(cohort3): combines two sequences and removes any duplicates\n");
+            Output(cohort2.Concat(cohort3),
+                "cohort2.Concat(cohort3): combines two sequences but leaves in any duplicates\n");
+            Output(cohort2.Intersect(cohort3),
+                "cohort2.Intersect(cohort3): returns items that are in both sequences\n");
+            Output(cohort2.Except(cohort3),
+                "cohort2.Except(cohort3): removes items from the first sequence that are in the second sequence\n");
+            Output(cohort1.Zip(
+                cohort2, (c1, c2) => $"{c1} matched with {c2}"),
+                "cohort1.Zip(cohort2, (c1, c2) => $\"{c1} matched with {c2}\"): matches items based on position in the sequence\n"
+                );
         }
     }
 }
---
+++ _vs2017/Chapter09/Ch09_Sets/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ch09_Sets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
