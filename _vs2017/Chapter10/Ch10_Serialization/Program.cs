f6ab22294607b355fcc969cce9c6a5ec554c134c
bnoden <brandroid369@gmail.com>
2017-04-28 18:49:16 -0500

    .

---
+++ _vs2017/Chapter10/Ch10_Serialization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ch10_Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
