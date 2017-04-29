6f02ce60c47e8f595e7d1f557c61b998a0f5a213
bnoden <brandroid369@gmail.com>
2017-04-28 23:42:15 -0500

    .

--- _vs2017/Chapter10/Ch10_Serialization/Program.cs
+++ _vs2017/Chapter10/Ch10_Serialization/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+using static System.Console;
+using Newtonsoft.Json;
 
 namespace Ch10_Serialization
 {
@@ -6,7 +11,49 @@ namespace Ch10_Serialization
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var people = new List<Person> {
+                new Person(30000M) {
+                FirstName = "Alice", LastName = "Smith",
+                DateOfBirth = new DateTime(1974, 3, 14) },
+            new Person(40000M) { FirstName ="Bob", LastName = "Jones",
+                DateOfBirth = new DateTime(1969, 11, 23) },
+            new Person(20000M) { FirstName = "Charlie", LastName = "Rose",
+                DateOfBirth = new DateTime(1964, 5, 4),
+                Children = new HashSet<Person> {
+                    new Person(0M) { FirstName = "Sally", LastName = "Rose",
+                    DateOfBirth = new DateTime(1990, 7, 12) } } }
+            };
+
+            string xmlFilepath = @"../Ch10_People.xml";
+            FileStream xmlStream = File.Create(xmlFilepath);
+
+            var xs = new XmlSerializer(typeof(List<Person>));
+
+            xs.Serialize(xmlStream, people);
+            xmlStream.Dispose();
+            WriteLine($"Written {new FileInfo(xmlFilepath).Length} bytes of XML to {xmlFilepath}");
+            WriteLine();
+
+            WriteLine(File.ReadAllText(xmlFilepath));
+
+            FileStream xmlLoad = File.Open(xmlFilepath, FileMode.Open);
+            var loadedPeople = (List<Person>)xs.Deserialize(xmlLoad);
+            foreach (var item in loadedPeople) {
+                WriteLine($"{item.LastName} has {item.Children.Count} children.");
+            }
+            xmlLoad.Dispose();
+
+            string jsonFilepath = @"../Ch10_People.json";
+            StreamWriter jsonStream = File.CreateText(jsonFilepath);
+            var jss = new JsonSerializer();
+
+            jss.Serialize(jsonStream, people);
+
+            jsonStream.Dispose();
+            WriteLine();
+            WriteLine($"Written {new FileInfo(jsonFilepath).Length} bytes of JSON to: {jsonFilepath}");
+
+            WriteLine(File.ReadAllText(jsonFilepath));
         }
     }
 }
