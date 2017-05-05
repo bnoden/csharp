2e160a4b66d773ba4b2be66f1d2b23572fe1ea8b
bnoden <brandroid369@gmail.com>
2017-05-04 19:57:22 -0500

    .

---
+++ _vs2017/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        double dp, t;
+
+        Console.Write("Enter the ratio D/P: ");
+
+        dp = Convert.ToDouble(Console.ReadLine());
+        t = (5730/Math.Log(2)*Math.Log(1+dp));
+
+        Console.WriteLine(
+            $"Given a D/P ratio of {dp} for C14, the age of the sample is {t:f2} years");
+    }
+};
